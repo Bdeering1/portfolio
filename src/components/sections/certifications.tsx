b300850f659c3427dd31d6d72709441161c6f701
Bryn Deering <bdeering@uoguelph.ca>
2025-05-17 18:57:40 -0400

    Improved certifications layout on mobile

--- src/components/sections/certifications.tsx
+++ src/components/sections/certifications.tsx
@@ -16,15 +16,15 @@ export default function Certifications() {
               target="_blank"
               rel="noopener"
             >
-              <div className="flex justify-between group py-2">
-                <h3 className="text-lg font-heading sm:text-xl">
+              <div className="flex justify-between gap-4 group py-2">
+                <h3 className="text-md sm:text-lg md:text-xl font-heading">
                   {cert.name}
                 </h3>
-                <div>
-                <h3 className="test-lg font-heading sm:text-xl">
-                  {cert.institution}
-                </h3>
-                <div className="bg-foreground h-[3px] w-0 group-hover:w-full transition-all duration-300"/>
+                <div className="flex-none">
+                  <h3 className="text-md sm:text:lg md:text-xl font-heading">
+                    {cert.institution}
+                  </h3>
+                  <div className="bg-foreground h-0.5 md:h-0.75 md:w-0 -translate-y-0.5 group-hover:w-full transition-all duration-300"/>
                 </div>
               </div>
             </a>
